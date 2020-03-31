211de03accce28ae215e587c5ad7099edc5a6c2d
Unknown <moreno6230@gmail.com>
2020-03-31 14:25:22 -0300

    implementacao das funcao primeiro estado

--- sarsa.jl
+++ sarsa.jl
@@ -1,10 +1,12 @@
 
 
-#lendos dados csv
+#""" lendos dados csv"""
 using DelimitedFiles
 using DataFrames
 using Random
-#CONSTANTES PARA OS ESTADOS
+
+"""CONSTANTES """
+#ESTADOS
 #estado 1
 const   LONG  = 1
 const   SHORT = 2
@@ -13,10 +15,12 @@ conjuntoPosicao = ["LONG", "SHORT", "NPOS"]
 #estados 2, 3, 5 e 6
 const   UP    = 1
 const   DOWN  = 2
+conjuntoVariacao = ["UP", "DOWN"]
 #estado 4
 const   BUY   = 1
 const   SELL  = 2
 const   NOPe   = 3
+conjuntoOperacao = ["BUY", "SELL", "NOPe"]
 #estado 7
 const   MAX   = 1
 const   MIN   = 2
@@ -31,13 +35,227 @@ const   STAY_SHORT  = 5
 const   EXIT_SHORT  = 6
 const   NOPa         = 7
 conjuntoAcao = ["ENTER_LONG", "STAY_LONG", "EXIT_LONG", "ENTER_SHORT", "STAY_SHORT", "EXIT_SHORT", "NOPa"]
-# Parametros
-qtd_Estados = 9
 
-# inicialização das variáveis
+"""FUNCOES"""
+# Funcao executa a acao
+function executarAcao(estado)
+      while true
+            acao = rand([1,2,3,4,5,6,7])
+            println("acao: ", conjuntoAcao[acao], " posicao: ", conjuntoPosicao[estado[1]])
+            if estado[1] == LONG
+                  if acao == STAY_LONG
+                        break
+                  elseif acao == EXIT_LONG
+                        estado[1] = NPOS
+                        break
+                  end
 
-estados = zeros(qtd_Estados)
-visitados = -ones(1728,7)
+            elseif estado[1] == SHORT
+                  if acao == STAY_SHORT
+                        break
+                  elseif acao == EXIT_SHORT
+                        estado[1] = NPOS
+                        break
+                  end
+            elseif estado[1] == NPOS
+                  if acao == ENTER_LONG
+                        estado[1] = LONG
+                        break
+                  elseif acao == ENTER_SHORT
+                        estado[1] = SHORT
+                        break
+                  elseif acao == NOPa
+                        break
+                  end
+            end
+      end
+end
+
+function printestado(estado)
+      if estado[1] == LONG
+            print("estado = [LONG, ")
+      elseif estado[1] == SHORT
+            print("estado = [SHORT, ")
+      elseif estado[1] == NPOS
+            print("estado = [NPOS, ")
+      end
+      if estado[2] == UP
+            print("UP, ")
+      else print("DOWN, ")
+      end
+      if estado[3] == UP
+            print("UP, ")
+      else print("DOWN, ")
+      end
+      if estado[4] == BUY
+            print("BUY, ")
+      elseif estado[4] == SELL
+            print("SELL, ")
+      else print("NOP, ")
+      end
+      if estado[5] == UP
+            print("UP, ")
+      else print("DOWN, ")
+      end
+      if estado[6] == UP
+            print("UP, ")
+      else print("DOWN, ")
+      end
+      if estado[7] == MAX
+            print("MAX, ")
+      else print("MIN, ")
+      end
+      if estado[8] == MAX
+            print("MAX, ")
+      else print("MIN, ")
+      end
+      if estado[9] == UP
+            print("UP]")
+      elseif estado[9] == DOWN
+             print("DOWN]")
+      else print("NPOS]")
+      end
+      println("")
+end
+
+function primeiroEstado(estado)
+      estado[1] = NPOS
+      #ESTADO 2
+      println(linhat1[4], linhat1[1], typeof(linhat1))
+      if (linhat1[4] - linhat1[1]) >= 0
+            estado[2] = UP
+      else estado[2] = DOWN
+      end
+      #ESTADO 3
+      if linhat2[4] - linhat2[1] >= 0
+            estado[3] = UP
+      else estado[3] = DOWN
+      end
+      #ESTADO 4
+      estado[4] = NOPa
+      #ESTADO 5
+      if linhat2[5] - linhat1[5] >= 0
+            estado[5] = UP
+      else estado[5] = DOWN
+      end
+      #ESTADO 6
+      if linhat3[5] - linhat2[5] >= 0
+            estado[6] = UP
+      else estado[6] = DOWN
+      end
+      #ESTADO 7
+      media = (linhat1[4] + linhat1[1])/2
+
+      if linhat1[2] >= media
+            estado[7] = MAX
+      else estado[7] = MIN
+      end
+      println("estado[7]: ", estado[7]," media: ", media," linha: ", linhat1[2])
+      #ESTADO 8
+      media = (linhat2[4] + linhat2[1])/2
+      if linhat2[2] >= media
+            estado[8] = MAX
+      else estado[8] = MIN
+      end
+      #ESTADO 9
+      estado[9] = NPOS
+end
+dados = open("dados/BPAC11.csv", "r")
+
+""" Estado inicial """
+historico = []
+estado = zeros(Int8,10)
+linhat1 = readline(dados)
+linhat1 = split(readline(dados),"\t")
+linhat1 = parse.(Float32,linhat1[3:8])
+linhat2 = split(readline(dados),"\t")
+linhat2 = parse.(Float32,linhat2[3:8])
+linhat3 = split(readline(dados),"\t")
+linhat3 = parse.(Float32,linhat3[3:8])
+
+primeiroEstado(estado)
+# estado[1] = NPOS
+# #ESTADO 2
+# println(linhat1[4], linhat1[1], typeof(linhat1))
+# if (linhat1[4] - linhat1[1]) >= 0
+#       estado[2] = UP
+# else estado[2] = DOWN
+# end
+# #ESTADO 3
+# if linhat2[4] - linhat2[1] >= 0
+#       estado[3] = UP
+# else estado[3] = DOWN
+# end
+# #ESTADO 4
+# estado[4] = NOPa
+# #ESTADO 5
+# if linhat2[5] - linhat1[5] >= 0
+#       estado[5] = UP
+# else estado[5] = DOWN
+# end
+# #ESTADO 6
+# if linhat3[5] - linhat2[5] >= 0
+#       estado[6] = UP
+# else estado[6] = DOWN
+# end
+# #ESTADO 7
+# media = (linhat1[4] + linhat1[1])/2
+#
+# if linhat1[2] >= media
+#       estado[7] = MAX
+# else estado[7] = MIN
+# end
+# println("estado[7]: ", estado[7]," media: ", media," linha: ", linhat1[2])
+# #ESTADO 8
+# media = (linhat2[4] + linhat2[1])/2
+# if linhat2[2] >= media
+#       estado[8] = MAX
+# else estado[8] = MIN
+# end
+# #ESTADO 9
+# estado[9] = NPOS
+println(estado)
+executarAcao(estado)
+printestado(estado)
+while true
+      acao = rand([1,2,3,4,5,6,7])
+      println("acao: ", conjuntoAcao[acao], " posicao: ", conjuntoPosicao[estado[1]])
+      if estado[1] == LONG
+            if acao == STAY_LONG
+                  break
+            elseif acao == EXIT_LONG
+                  estado[1] = NPOS
+                  break
+            end
+
+      elseif estado[1] == SHORT
+            if acao == STAY_SHORT
+                  break
+            elseif acao == EXIT_SHORT
+                  estado[1] = NPOS
+                  break
+            end
+      elseif estado[1] == NPOS
+            if acao == ENTER_LONG
+                  estado[1] = LONG
+                  break
+            elseif acao == ENTER_SHORT
+                  estado[1] = SHORT
+                  break
+            elseif acao == NOPa
+                  break
+            end
+      end
+end
+
+# linhat1 = readline(dados)
+# linha = readline(dados)
+#
+# while !eof(dados)
+#       linha = readline(dados)
+#       #<DATE>	<TIME>	<OPEN>	<HIGH>	<LOW>	<CLOSE>	<TICKVOL>	<VOL>	<SPREAD>
+#       println(linha)
+# end
 
 # function recompensa (estado, acao)
 #         if
@@ -56,50 +274,3 @@ visitados = -ones(1728,7)
 #         println(linha)
 #         #println(historico[linha])
 # end
-
-dados = open("dados/BPAC11.csv", "r")
-estado = zeros(Int8,9)
-linhat1 = readline(dados)
-linhat1 = readline(dados)
-estado[1] = NPOS
-linhat2 = readline(dados)
-bool = false
-while !bool
-      acao = rand([1,2,3,4,5,6,7])
-      println("acao: ", conjuntoAcao[acao], " posicao: ", conjuntoPosicao[estado[1]])
-      if estado[1] == LONG
-            if acao == STAY_LONG
-                  bool = true
-            elseif acao == EXIT_LONG
-                  bool = true
-                  estado[1] = NPOS
-            end
-
-      elseif estado[1] == SHORT
-            if acao == STAY_SHORT
-                  bool = true
-            elseif acao == EXIT_SHORT
-                  bool = true
-                  estado[1] = NPOS
-            end
-      elseif estado[1] == NPOS
-            if acao == ENTER_LONG
-                  bool = true
-                  estado[1] = LONG
-            elseif acao == ENTER_SHORT
-                  bool = true
-                  estado[1] = SHORT
-            elseif acao == NOPa
-                  bool = true
-            end
-      end
-end
-
-linhat1 = readline(dados)
-linha = readline(dados)
-
-while !eof(dados)
-      linha = readline(dados)
-      #<DATE>	<TIME>	<OPEN>	<HIGH>	<LOW>	<CLOSE>	<TICKVOL>	<VOL>	<SPREAD>
-      println(linha)
-end
